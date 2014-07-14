33fbea0556b6bab209be0cdbb741ee17d51aca29
arodgersHPS <andrew.rodgers@e-hps.com>
2014-07-14 09:10:18 -0400

    Added End to End NET example

---
+++ NET/End-to-End/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="End_to_End.MvcApplication" Language="C#" %>
---
+++ NET/End-to-End/Models/userModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace End_to_End.Models
+{
+    public class UsersContext : DbContext
+    {
+
+        public class UserModel
+        {
+            public decimal Amount { get; set; }
+            public int cvv { get; set; }
+            public int expMonth { get; set; }
+            public int expYear { get; set; }
+            public string number { get; set; }
+            public string address { get; set; }
+            public string zipcode { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string phoneNumber { get; set; }
+            public string Email { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+
+        }
+
+    }
+
+
+}
