195f9a8608c96a56b4dfe036997fc409044702b5
clayton.hunt <clayton.hunt@e-hps.com>
2014-07-30 13:43:29 -0400

    removed nuget packages that were not needed

--- mvc.net/end-to-end/Global.asax.cs
+++ mvc.net/end-to-end/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace end_to_end
@@ -12,13 +11,12 @@ namespace end_to_end
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
